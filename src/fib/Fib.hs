19472e79b453232ed25ed9815a1a6234921b2e86
rashadg1030 <rashad.sasaki@gmail.com>
2019-02-12 21:56:47 -0800

    Solution for Fib complete but is too slow.

--- src/fib/Fib.hs
+++ src/fib/Fib.hs
@@ -1,13 +1,34 @@
 module Fib where
 
+data Rabbit = Adult | Kitten
+    deriving (Show, Eq)
+
+tick :: Int -> Rabbit -> [Rabbit]
+tick k Adult  = [Adult] ++ replicate k Kitten
+tick k Kitten = [Adult] 
+
+tock :: [Rabbit] -> Int -> Int -> [Rabbit]
+tock [] _ _     = []
+tock rs n b
+    | n == 1    = rs
+    | otherwise = tock (concatMap (tick b) rs) (n-1) b
+    
+initFarm :: (Int, Int) -> [Rabbit]
+initFarm = uncurry $ tock [Kitten]
+
+awkward :: [Int] -> (Int,Int)
+awkward [x,y] = (x,y)
+awkward _     = (0,0)
+
 solve :: IO ()
 solve = do
     -- File read can be awkward
     line <- readFile "rosalind_fib.txt"
-    let solution = (read <$> words line) :: [Int]
-    print solution
+    let solution = length $ initFarm $ awkward $ read <$> words line  
+    writeFile "fib_solution.txt" (show solution) 
 
 
--- kSequence :: Int -> [Int] 
--- kSequence
--- kSequence k = k : kSequence k'
+kSequence :: [Int]
+kSequence = 0 : 3 : rem kSequence
+  where
+    rem (a : t@(b:_)) = (a+b) : rem t
